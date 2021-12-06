84d196eba5ab5070026abba741809e6a59bd651d
Vladimir-Shevchenko <0982124886lm@gmail.com>
2021-12-06 15:34:38 +0200

    HW5 Spring Boot

--- src/main/java/com/example/SpringBoot/HealthCheck/HealthCheck.java
+++ src/main/java/com/example/SpringBoot/HealthCheck/HealthCheck.java
@@ -1,4 +1,4 @@
-package com.example.SpringBoot.service;
+package com.example.SpringBoot.HealthCheck;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
